8060b508787cfe2e21c54f31f52b733b8028527f
Wynchemna <felix.radkte@gmx.de>
2022-01-12 16:42:18 +0100

    fertig

--- 03_skripts/Ex1.R
+++ 03_skripts/Ex1.R
@@ -7,7 +7,12 @@ library(tidyverse)
 library(lubridate)
 
 # ---- load raw data ----
+# Surface
 data_raw <- read_csv(file = 'C:/Users/radtk/OneDrive/Dokumente/Data_management_storage/data_management_hobo/01_data_raw/hobo/2022/raw/10347355.csv', skip = 1, col_names = T) 
+
+# Stand PC
+data_raw <- read_csv(file = 'A:/radtk/OneDrive/Dokumente/Data_management_storage/data_management_hobo/01_data_raw/hobo/2022/raw/10347355.csv', skip = 1, col_names = T) 
+
 
 #janitor::clean_names(data) if skip = 1
 
@@ -16,7 +21,12 @@ names(data_raw) <- col_names
 
 
 # ---- save data with clean names ----
-write_csv(x=data_raw, file = 'C:/Users/radtk/OneDrive/Dokumente/Data_management_storage/data_managemant_hobo/02_data_processed/10347355_cleannames.csv')
+#Surface
+write_csv(x=data_raw, file = 'C:/Users/radtk/OneDrive/Dokumente/Data_management_storage/data_management_hobo/02_data_processed/10347355_cleannames.csv')
+
+# Stand PC
+write_csv(x=data_raw, file = 'A:/radtk/OneDrive/Dokumente/Data_management_storage/data_management_hobo/02_data_processed/10347355_cleannames.csv')
+
 
 
 # ---- look at the data ----
@@ -40,20 +50,22 @@ plot(x = data$id, y = data$lux, type = 'l') #lux
 # ---- filter data to 4 full weeks ----
 
 data <- data_raw %>% 
-	mutate(dttm = dmy_hms(dttm)) %>% 
-	mutate(dttm_2 = substring(dttm, 1, 10),
-	       dttm_2 = ymd(dttm_2),
-	       dttm_2 = ymd(dttm_2)) %>% 
+	select(id, dttm, temp, lux) %>% 
+	mutate(dttm = dmy_hms(dttm, quiet = T)) %>% 
+	mutate(date = date(dttm)) %>% 
 	mutate(timestamp = substring(dttm, 12, 19),
 	       timestamp = hms(timestamp)) %>% 
-	filter('2021-12-12' < dttm_2 & dttm_2 < '2022-01-10') %>% 
-	#filter(dttm_2 == '2022-01-09' & timestamp < '23H 10M 0S') %>% 
+	filter('2021-12-12' < date & date < '2022-01-10') %>%
+	mutate(id = row_number(id)) %>% 
+	mutate(dttm = format(dttm, '%Y-%m-%d %H:%M:%S')) %>% 
 	select(id, dttm, temp, lux)
 
 # ---- save data with clean names ----
-write_csv(x=data, file = 'C:/Users/radtk/OneDrive/Dokumente/Data_management_storage/data_managemant_hobo/02_data_processed/10347355.csv')
+#Surface
+write_csv(x=data, file = 'C:/Users/radtk/OneDrive/Dokumente/Data_management_storage/data_management_hobo/02_data_processed/10347355.csv')
 
-
+#Stand-PC
+write_csv(x=data, file = 'A:/radtk/OneDrive/Dokumente/Data_management_storage/data_management_hobo/02_data_processed/10347355.csv')
 
 
 
